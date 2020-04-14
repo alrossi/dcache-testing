373d48ccdf66b017988032d88282847273a6e047
Albert Louis Rossi <arossi@fnal.gov>
2020-04-14 11:52:55 -0500

    recursive mkdir

--- bin/testbed-pnfs-qos.sh
+++ bin/testbed-pnfs-qos.sh
@@ -1,33 +1,41 @@
 #!/bin/bash
 
 DEPTH=3
+LEVEL=0
+DIR=
 
 make_leaves() {
-	if [ $1 -eq $DEPTH ] ; then
+	if [ $LEVEL -eq $DEPTH ] ; then
 		return
-        fi
+    fi
+	local parent=$DIR
 	for d in `seq 0 4`
 	do
-  		mkdir $d
+  		mkdir  $d
 		cd $d
-		make_leaves $(( $1 - 1 ))
+		DIR=$parent/$d
+		echo $DIR
+		LEVEL=$(( $LEVEL + 1 ))
+		make_leaves
+		LEVEL=$(( $LEVEL - 1 ))
 		cd ..
 	done
+	DIR=$parent
 }
 
-mkdir -p /pnfs/fs/usr/test/arossi/volatile
-cd /pnfs/fs/usr/test/arossi/volatile
-pwd
-echo "NEARLINE" > ".(tag)(AccessLatency)"
-echo "REPLICA" > ".(tag)(RetentionPolicy)"
-echo "dcache-devel-test" > ".(tag)(file_family)"
-echo "10" > ".(tag)(file_family_width)"
-echo "volatile" > ".(tag)(storage_group)"
+# mkdir -p /pnfs/fs/usr/test/arossi/volatile
+# cd /pnfs/fs/usr/test/arossi/volatile
+# pwd
+# echo "NEARLINE" > ".(tag)(AccessLatency)"
+# echo "REPLICA" > ".(tag)(RetentionPolicy)"
+# echo "dcache-devel-test" > ".(tag)(file_family)"
+# echo "10" > ".(tag)(file_family_width)"
+# echo "volatile" > ".(tag)(storage_group)"
 
 make_leaves
 
 
-cd /pnfs/fs/usr/test/arossi
-chown -R 8773 .
-chgrp -R 1530 .
-chmod -R ugo+rwx .
+# cd /pnfs/fs/usr/test/arossi
+# chown -R 8773 .
+# chgrp -R 1530 .
+# chmod -R ugo+rwx .
